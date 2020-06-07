cda60cd2c9c9ed89cf65b9147d84ed0af04fcf73
Itay113 <itayadler13@gmail.com>
2020-06-07 22:22:12 +0300

    initial commit

---
+++ Assets/Assets/Scripts/EnemyController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    public Transform enemyCheck;
+    public Transform playerCheck;
+
+    public Transform player;
+    private Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    private void FixedUpdate()
+    {
+        if(Mathf.Abs(enemyCheck.position.x) == Mathf.Abs(playerCheck.position.x)){
+            Debug.Log("Touched");
+            Destroy(this.gameObject);
+        }
+    }
+}
---
+++ Assets/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Animator anim;
+
+    public Transform groundCheck;
+    public LayerMask groundObjects;
+
+    public float movementSpeed;
+    private float moveDirection;
+    public float jumpForce;
+    public float checkRadius;
+
+    private Rigidbody2D rb;
+    
+    private bool facingRight = true;
+    private bool isJumping = false;
+    private bool isGrounded;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        anim.SetFloat("Speed", Mathf.Abs(moveDirection));
+
+        // Getting the player inputs:
+        Inputs();
+
+        // Fliping the rotation of the player character, right and left:
+        FlipingCharacter();
+        
+    }
+    
+    private void FixedUpdate(){
+        Move();
+
+        // Ground check:
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, checkRadius, groundObjects);
+    }
+
+    private void Inputs(){
+        moveDirection = Input.GetAxis("Horizontal");
+        if (Input.GetButtonDown("Jump") && isGrounded){
+            Debug.Log("jump");
+            isJumping = true;
+            anim.SetBool("IsJumping", true);
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D player){
+        if (player.collider.tag == "Ground"){
+            anim.SetBool("IsJumping", false);
+        }
+    }
+
+    private void Move(){
+        rb.velocity = new Vector2(moveDirection * movementSpeed, rb.velocity.y);
+        if(isJumping){
+            rb.velocity = new Vector2(0f,jumpForce);
+        }
+        isJumping = false;
+    }
+
+    private void FlipingCharacter(){
+        if(moveDirection > 0 && !facingRight){
+            Flip();
+        }
+        else if(moveDirection < 0 && facingRight){
+            Flip();
+        }
+    }
+
+    private void Flip()
+    {
+        facingRight = !facingRight;
+        transform.Rotate(0f,180f,0f);
+    }
+}
