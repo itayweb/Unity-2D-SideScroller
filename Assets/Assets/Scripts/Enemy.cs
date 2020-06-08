4ab1a6f316937d6f17e79b84d8be643478116494
adimations <itayadler7@gmail.com>
2020-06-08 15:31:32 +0300

    initial commit

---
+++ Assets/Assets/Scripts/Enemy.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    private Animator anim;
+
+    public int maxHealth = 50;
+    public int currentHealth;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        currentHealth = maxHealth;
+    }
+    
+    public void TakeDamage(int playerAttackDamage){
+        // Substracting the current health with the attack damage:
+        currentHealth -= playerAttackDamage;
+
+        // Playing enemy hurt animation:
+        anim.SetTrigger("Hurt");
+        
+        // Killing the enemy and die:
+        if(currentHealth <= 0){
+            Die();
+        }
+    }
+
+    void Die(){
+        Debug.Log("Enemy died");
+
+        // Playing die animation:
+        anim.SetBool("IsDead", true);
+
+        // Disable the enemy:
+        GetComponent<Rigidbody2D>().simulated = false;
+        GetComponent<Collider2D>().enabled = false;
+        this.enabled = false;
+    }
+}
---
+++ Assets/Assets/Scripts/PlayerCombat.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCombat : MonoBehaviour
+{
+    public int attackDamage = 10;
+
+    public Transform attackPoint;
+    public float attackRange = 0.5f;
+    public LayerMask enemiesLayer;
+
+    private Animator anim;
+
+    private void Start() {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown("mouse 0") || Input.GetKeyDown("left ctrl")){
+            Attack();
+        }
+    }
+
+    void Attack(){
+        // Playing the attack animation:
+        anim.SetTrigger("Attack");
+
+        // Detecting the enemies:
+        Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(attackPoint.position, attackRange,enemiesLayer);
+        foreach(Collider2D enemy in hitEnemies){
+            enemy.GetComponent<Enemy>().TakeDamage(attackDamage);
+        }
+    }
+    
+    private void OnDrawGizmosSelected() {
+        if(attackPoint == null){
+            return;
+        }
+
+        Gizmos.DrawWireSphere(attackPoint.position, attackRange);
+    }
+}
